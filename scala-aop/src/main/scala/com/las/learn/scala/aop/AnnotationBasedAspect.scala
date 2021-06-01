b48c1a417e6b95e959a4ab4f600f9529fc442831
liansheng <liansheng@daojia-inc.com>
2021-06-01 15:22:11 +0800

    scala aop

---
+++ scala-aop/src/main/scala/com/las/learn/scala/aop/AnnotationBasedAspect.scala
@@ -0,0 +1,31 @@
+package com.las.learn.scala.aop
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.LoggerFactory
+
+/** @author: liansheng
+  * @Description:
+  */
+@Aspect
+class AnnotationBasedAspect {
+
+  /** around execution of Sum
+    */
+  @Around("execution(@com.knoldus.aspect.Loggable * *.*(..))")
+  def aroundSum(joinPoint: ProceedingJoinPoint): Object = {
+    val result = joinPoint.proceed
+    println(
+      "Method:-" + joinPoint
+        .toShortString() + " Input:-" + joinPoint.getArgs().toList.mkString(",") + " Result:-" + result
+    )
+    result
+  }
+
+}
+
+class Sum {
+  @Loggable
+  def checkSum(a: Int, b: Int): Int = a + b
+}
---
+++ scala-aop/src/main/scala/com/las/learn/scala/aop/Loggable.java
@@ -0,0 +1,8 @@
+package com.las.learn.scala.aop;
+
+/**
+ * @author: liansheng
+ * @Description:
+ */
+public @interface Loggable {
+}
